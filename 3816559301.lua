dec8da5533a828c41796881fb86b2a59287022dc
aesthetic <164383159+V0lsheBstvo@users.noreply.github.com>
2025-05-10 19:28:02 +0300

    Update 3816559301.lua

--- 3816559301.lua
+++ 3816559301.lua
@@ -1,28 +1,28 @@
-return {
-    name = "Just Afk Game",
-    init = function(env)
-        env.Section:NewButton(
-            "Открыть VIP",
-            "Доступ в VIP зону",
-            function()
-                local door = env.Workspace:FindFirstChild("VipDoor")
-                if door then
-                    door.CanCollide = false
-                    door.Transparency = 0.8
-                end
+return function(env)
+    -- Создаем секцию автоматически
+    local mainSection = env.Main_Tab:NewSection("Just AFK Game idk")
+    
+    mainSection:NewButton(
+        "Открыть VIP",
+        "Доступ в VIP зону",
+        function()
+            local door = env.Workspace:FindFirstChild("VipDoor")
+            if door then
+                door.CanCollide = false
+                door.Transparency = 0.8
             end
-        )
+        end
+    )
 
-        env.Section:NewButton(
-            "Открыть 1000Min",
-            "Секретная дверь",
-            function()
-                local door = env.Workspace:FindFirstChild("1000MinDoor")
-                if door then
-                    door.CanCollide = false
-                    door.Transparency = 0.7
-                end
+    mainSection:NewButton(
+        "Открыть 1000Min",
+        "Секретная дверь",
+        function()
+            local door = env.Workspace:FindFirstChild("1000MinDoor")
+            if door then
+                door.CanCollide = false
+                door.Transparency = 0.7
             end
-        )
-    end
-}
+        end
+    )
+end
