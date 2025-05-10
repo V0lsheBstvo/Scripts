854011512f9aaa2726680103dd1d95cfe6b6bda5
aesthetic <164383159+V0lsheBstvo@users.noreply.github.com>
2025-05-10 19:08:09 +0300

    Update 3816559301.lua

--- 3816559301.lua
+++ 3816559301.lua
@@ -1,1 +1,28 @@
-111
+return function()
+    gameScript.name = "Just Afk Game"
+
+    Section:NewButton(
+        "Открыть VIP",
+        "Доступ в VIP зону",
+        function()
+            local door = Workspace:FindFirstChild("VipDoor")
+            if door then
+                door.CanCollide = false
+                door.Transparency = 0.8
+            end
+        end
+    )
+
+    Section:NewButton(
+        "Открыть 1000Min",
+        "Секретная дверь",
+        function()
+            local door = Workspace:FindFirstChild("1000MinDoor")
+            if door then
+                door.CanCollide = false
+                door.Transparency = 0.7
+            end
+        end
+    )
+  
+end
